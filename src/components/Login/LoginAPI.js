88fae38a2ea2d0025516f510831b81510ef27458
Jonas Sperling <76401299+Jonyser@users.noreply.github.com>
2022-03-13 15:49:51 +0200

    First attempt to get the token of new login

--- src/components/Login/LoginAPI.js
+++ src/components/Login/LoginAPI.js
@@ -4,34 +4,53 @@ function LoginAPI(props) {
     const [urlToLogin, setUrlToLogin] = useState('')
 
 
+    fetch('http://s83.bfa.myftpupload.com/wp-json/jwt-auth/v1/token',{
+    method: "POST",
+    headers:{
+        'Content-Type': 'application/json',
+        'accept': 'application/json',
+    },
 
-    useEffect(() => {
-        if (props.APIDetailsLogin.user.length > 0) {
-            let formData = new FormData()
-            formData.append('email', props.APIDetailsLogin.user)
-            formData.append('password', props.APIDetailsLogin.pass)
-            const url = 'https://s83.bfa.myftpupload.com/?rest_route=/simple-jwt-login/v1/auth'
-            fetch(url, {
-                method: 'POST',
-                body: formData
-            })
-                .then((response) => response.json())
-                .then((data) => {
-                    if (data['success'] == true) {
+    body:JSON.stringify({
+        username: 'jonassp@post.jce.ac.il',
+        password: 'GvS7GZJUDLt0DKBM'
+    })
+    }).then(function(response){
+        return response.json()
+    }).then(function(user){
+        console.log(user.token)
+        localStorage.setItem('jwt', user.token)
+    });
 
-                        console.log(data)
-                        localStorage.setItem('jwt', data['data']['jwt'])
 
-                        setUrlToLogin(`https://s83.bfa.myftpupload.com/?rest_route=/simple-jwt-login/v1/autologin&JWT=${data['data']['jwt']}`)
-                    }
-                    else {
-                        console.log(data)
-                        props.setServerMessage(data['data']['message'])
-                    }
-                })
-        }
 
-    }, [props.APIDetailsLogin])
+    // useEffect(() => {
+    //     if (props.APIDetailsLogin.user.length > 0) {
+    //         let formData = new FormData()
+    //         formData.append('email', props.APIDetailsLogin.user)
+    //         formData.append('password', props.APIDetailsLogin.pass)
+    //         const url = 'https://s83.bfa.myftpupload.com/?rest_route=/simple-jwt-login/v1/auth'
+    //         fetch(url, {
+    //             method: 'POST',
+    //             body: formData
+    //         })
+    //             .then((response) => response.json())
+    //             .then((data) => {
+    //                 if (data['success'] == true) {
+
+    //                     console.log(data)
+    //                     localStorage.setItem('jwt', data['data']['jwt'])
+
+    //                     setUrlToLogin(`https://s83.bfa.myftpupload.com/?rest_route=/simple-jwt-login/v1/autologin&JWT=${data['data']['jwt']}`)
+    //                 }
+    //                 else {
+    //                     console.log(data)
+    //                     props.setServerMessage(data['data']['message'])
+    //                 }
+    //             })
+    //     }
+
+    // }, [props.APIDetailsLogin])
 
     return (
         <>
